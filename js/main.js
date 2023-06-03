92a2343f09a274c569ad3454773dbcc0fb1c52ff
gaillardnico <h.nicolasgaillard@gmail.com>
2023-06-03 10:34:16 -0300

    Mejorando la function logueo para evitar el ingreso de numeros y agregando precios, metodos de pagos e informacion

--- js/main.js
+++ js/main.js
@@ -1,52 +1,74 @@
 // Comienzo de la primer pre entrega JS
 //Declarando variables
-let nombreUsuario 
+let nombreUsuario
 let apellidoUsuario
 let valor
-
-
-
+let mother = 42500
+let procesador = 69800
+let ram = 25000
+let totalPrecio = ram + procesador + mother
+let tresCtasMyV = totalPrecio * 1.15
+let seisCtasMyV = totalPrecio * 1.30
+let tresCtasMax = totalPrecio * 1.10
 
 //"Logueo" del usuario que determina si puede acceder al menú
-nombreUsuario = prompt("Ingrese su nombre")
-apellidoUsuario = prompt("Ahora ingrese su apellido")
-
-// Condicional respecto al "Logueo" anterior para acceder al menú
-if (((nombreUsuario != "") && (apellidoUsuario != "")) && ((nombreUsuario != null) && (apellidoUsuario != null))){
-    alert(`Bienvenido ${nombreUsuario} ${apellidoUsuario} a la tienda de componentes de PC.
-    A continuación le mostraremos una serie de opciones enumeradas.`)
-    //llamado a la función menú
-    menu()
-}else {
-    alert("No ingresó correctamente Nombre y/o Apellido, vuelva a intentarlo.")
+logueo()
+function logueo() {
+    nombreUsuario = prompt("Ingrese su nombre")
+    apellidoUsuario = prompt("Ahora ingrese su apellido")
+    // Condicional respecto al "Logueo" anterior para acceder al menú
+    if (((nombreUsuario != "" && apellidoUsuario != "") && (nombreUsuario != null && apellidoUsuario != null)) && (isNaN(nombreUsuario) && isNaN(apellidoUsuario))) {
+        alert(`Bienvenido ${nombreUsuario} ${apellidoUsuario} a la tienda de componentes de PC.
+A continuación le mostraremos una serie de opciones enumeradas.`)
+        //llamado a la función menú
+        menu()
+    } else {
+        alert("No ingresó correctamente Nombre y/o Apellido, vuelva a intentarlo.")
+        //Llamado a funcion logueo para reingresar nombre y apellido
+        logueo()
+    }
 }
 
 
 //Funcion menú
-
 function menu() {
     valor = prompt(`Ingrese un número según la opción a la que desee ingresar:
     1. Lista de componentes
-    2. Estimador de costos
+    2. Metodos de pago
     3. Información extra
     0. Salir del Menú`)
 
-    while (valor != "ESC") {
+    while (valor != "0") {
         switch (valor) {
             case "1":
-                alert(`Mother Asus `)
-            break
+                alert(`Los componentes disponibles son:
+Mother Asus $${mother}
+Procesador intel $${procesador}
+Memoria ram 16gb DDR4 $${ram}
+
+El total de la suma de los componentes es $${totalPrecio}`)
+                break
             case "2":
-                alert("El producto seleccionado es PAPA")
-            break
+                alert(`Los metodos de pagos son:
+-Efectivo, transferencia o débito $${totalPrecio}
+
+-VISA o MasterCard: 
+-1 pago sin interés
+-3 cuotas con un 15% $${tresCtasMyV}
+-6 cuotas con un 30% $${seisCtasMyV}
+
+Naranja PLAN Z:
+-11 cuotas con 15% $${tresCtasMyV}
+
+Consumax PLAN MAX:
+-3 cuotas con un 10% $${tresCtasMax}`)
+                break
             case "3":
-                alert("El producto seleccionado es CARNE")
-            break
-            case "0":
-                valor = "ESC"
+                alert("Este combo permite al usuario utilizar unos componentes de maxima calidad y rendimiento a un costo relativamente accesible.")
+                break
             default:
                 alert("ERROR, ingrese un número según corresponda")
-            break
+                break
         }
         valor = prompt(`Ingrese un número según la opción a la que desee ingresar:
         1. Lista de componentes
@@ -54,10 +76,8 @@ function menu() {
         3. Información extra
         0. Salir del Menú`)
     }
+    alert("Gracias por visitar nuestro sitio.")
 }
-
-
-
 
 
 
